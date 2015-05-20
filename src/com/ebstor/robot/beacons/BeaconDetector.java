093604c014ab2372932950f81907599baabb27dc
3bst0r <johannes.ebster@student.uibk.ac.at>
2015-05-20 18:45:39 +0200

    introduced diameter check

--- src/com/ebstor/robot/beacons/BeaconDetector.java
+++ src/com/ebstor/robot/beacons/BeaconDetector.java
@@ -18,18 +18,17 @@ public class BeaconDetector {
 
     private List<Beacon> beaconsDetected;
     private ColorBlobDetector blobDetector;
-    private Map<BeaconColor,List<BeaconContour>> beaconContours;
+    private Map<BeaconColor, List<BeaconContour>> beaconContours;
 
 
     /**
-     *
      * @return 2 neighboring beacons or null if there are not enough beacons seen
      */
     public Pair<Beacon, Beacon> getBeacons() {
         // enums are ordered by their ordinals, this works as long as they are ordered correctly in the declaration
         Collections.sort(beaconsDetected);
         try {
-            return new Pair<>(beaconsDetected.get(0),beaconsDetected.get(1));
+            return new Pair<>(beaconsDetected.get(0), beaconsDetected.get(1));
         } catch (IndexOutOfBoundsException e) {
             return null;
         }
@@ -39,76 +38,68 @@ public class BeaconDetector {
         this.beaconsDetected = new LinkedList<>();
         this.blobDetector = blobDetector;
         this.beaconContours = new EnumMap<>(BeaconColor.class);
-        for (BeaconColor color: BeaconColor.values())
-            beaconContours.put(color,new LinkedList<BeaconContour>());
+        for (BeaconColor color : BeaconColor.values())
+            beaconContours.put(color, new LinkedList<BeaconContour>());
     }
 
 
     public void process(Mat rgbaImage) {
         beaconsDetected.clear();
         BeaconColor[] values = BeaconColor.values();
-        for (BeaconColor beaconColor: values) {
+        for (BeaconColor beaconColor : values) {
             Scalar hsvColor = beaconColor.hsvColor();
             blobDetector.setHsvColor(hsvColor);
             blobDetector.process(rgbaImage);
             List<MatOfPoint> contours = blobDetector.getContours();
             List<BeaconContour> beaconContours = this.beaconContours.get(beaconColor);
-            for (MatOfPoint contour: contours)
+            for (MatOfPoint contour : contours)
                 beaconContours.add(new BeaconContour(contour));
         }
         /* compare every red contour with every (differently colored) contour */
         List<BeaconContour> redContours = beaconContours.get(BeaconColor.RED);
-        for (int i = 0; i < values.length; i++) {
-            if (values[i] != BeaconColor.RED) {
-                for (BeaconContour beaconContour2 : beaconContours.get(values[i])) {
-                    for (BeaconContour beaconContour: redContours) {
+        for (int i = 0; i < values.length; i++)
+            if (values[i] != BeaconColor.RED)
+                for (BeaconContour beaconContour2 : beaconContours.get(values[i]))
+                    for (BeaconContour beaconContour : redContours) {
                         int comp = compareContours(beaconContour.getTriple(), beaconContour2.getTriple());
                         if (comp != 0) {
                             Beacon beaconRed = findRedBeacons(comp, values[i], beaconContour.getTriple(), beaconContour2.getTriple());
-                            if (beaconRed != null) {
+                            if (beaconRed != null)
                                 beaconsDetected.add(beaconRed);
-                            }
                         }
                     }
-                }
-            }
-        }
+
         /* compare every blue contour with every (differently colored) contour */
-        List <BeaconContour> blueContours = beaconContours.get(BeaconColor.BLUE);
-        for (int i = 0; i < values.length; i++) {
-            if (values[i] != BeaconColor.BLUE) {
-                for (BeaconContour beaconContour2 : beaconContours.get(values[i])) {
-                    for (BeaconContour beaconContour: blueContours) {
+        List<BeaconContour> blueContours = beaconContours.get(BeaconColor.BLUE);
+        for (int i = 0; i < values.length; i++)
+            if (values[i] != BeaconColor.BLUE)
+                for (BeaconContour beaconContour2 : beaconContours.get(values[i]))
+                    for (BeaconContour beaconContour : blueContours) {
                         int comp = compareContours(beaconContour.getTriple(), beaconContour2.getTriple());
                         if (comp != 0) {
                             Beacon beaconBlue = findBlueBeacons(comp, values[i], beaconContour.getTriple(), beaconContour2.getTriple());
-                            if (beaconBlue != null) {
+                            if (beaconBlue != null)
                                 beaconsDetected.add(beaconBlue);
-                            }
                         }
                     }
-                }
-            }
-        }
     }
 
 
     private Beacon findRedBeacons(int comp, BeaconColor color, Point[] redTriple, Point[] triple2) {
         // if comp < 0 then color is above red, triple2 is above triple1
         Beacon result = null;
-        switch(color) {
+        switch (color) {
             case BLUE:
                 if (comp < 0) {
                     result = Beacon.BLUE_RED;
                     result.egocentricCoordinates = ColorBlobDetectionActivity.imageCoordToEgoCoord(redTriple[0]);
-                }
-                else {
-                   result = Beacon.RED_BLUE;
+                } else {
+                    result = Beacon.RED_BLUE;
                     result.egocentricCoordinates = ColorBlobDetectionActivity.imageCoordToEgoCoord(triple2[0]);
                 }
                 break;
             case PURPLE:
-                if (comp < 0 ) {
+                if (comp < 0) {
                     result = Beacon.PURPLE_RED;
                     result.egocentricCoordinates = ColorBlobDetectionActivity.imageCoordToEgoCoord(redTriple[0]);
                 }
@@ -117,8 +108,7 @@ public class BeaconDetector {
                 if (comp < 0) {
                     result = Beacon.BLACK_RED;
                     result.egocentricCoordinates = ColorBlobDetectionActivity.imageCoordToEgoCoord(redTriple[0]);
-                }
-                else {
+                } else {
                     result = Beacon.RED_BLACK;
                     result.egocentricCoordinates = ColorBlobDetectionActivity.imageCoordToEgoCoord(triple2[0]);
                 }
@@ -139,8 +129,7 @@ public class BeaconDetector {
                 if (comp < 0) {
                     result = Beacon.BLACK_BLUE;
                     result.egocentricCoordinates = ColorBlobDetectionActivity.imageCoordToEgoCoord(blueTriple[0]);
-                }
-                else {
+                } else {
                     result = Beacon.BLUE_BLACK;
                     result.egocentricCoordinates = ColorBlobDetectionActivity.imageCoordToEgoCoord(triple2[0]);
                 }
@@ -188,14 +177,22 @@ public class BeaconDetector {
     } */
 
     /**
-     *
      * @return -1 if triple2 is above triple1, 1 if triple2 is below triple1, 0 if contours do not belong to same beacon
      */
     private int compareContours(Point[] triple1, Point[] triple2) {
-        double areaX = Math.abs(triple1[1].x - triple1[2].x)/2;
-        double x = Math.abs(triple2[1].x - triple2[2].x)/2 + triple2[1].x;
-        if (x > (triple1[1].x - areaX) && x < (triple1[1].x + 2 * areaX))
-            return Double.compare(triple1[0].y,triple2[0].y);
+        /* check if their diameters are similar
+        * if one diameter is less than 75% of the other, the contours do not belong to one beacon*/
+        double diameter1 = Math.abs(triple1[1].x - triple1[2].x);
+        double diameter2 = Math.abs(triple2[1].x - triple2[2].x);
+        if (diameter1/diameter2 < 0.75 || diameter2/diameter1 < 0.75) return 0;
+
+        /* TODO check if the contours adjoin */
+
+        /* check if they are in the same x area */
+        double diameterX = diameter1/ 2;
+        double x = Math.abs(triple2[1].x - triple2[2].x) / 2 + triple2[1].x;
+        if (x > (triple1[1].x - diameterX) && x < (triple1[1].x + 2 * diameterX))
+            return Double.compare(triple1[0].y, triple2[0].y);
         else
             return 0;
     }
